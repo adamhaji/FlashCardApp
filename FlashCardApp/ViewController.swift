badea29d46bb384b7f17edcb7561071f94f1fcb8
adamhaji <adamhaji@icloud.com>
2022-09-19 23:24:17 -0700

    FlashCard app complete

--- FlashCardApp/ViewController.swift
+++ FlashCardApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var answer: UILabel!
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var card: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 15.0
+        card.clipsToBounds = true
+        card.layer.shadowRadius = 10.0
+        card.layer.shadowOpacity = 0.3
     }
 
-
+    @IBAction func didTap(_ sender: Any) {
+        // question.isHidden = true
+        if (answer.isHidden) {
+            question.isHidden = true
+            answer.isHidden = false
+        } else {
+            answer.isHidden = true
+            question.isHidden = false
+        }
+    }
+    
+    
+    
 }
 
